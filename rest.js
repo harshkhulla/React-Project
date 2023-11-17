f0f0971d89eaa0db0b8ac1f51f127b914b1e9f65
Harsh Khullar <103531236+harshkhulla@users.noreply.github.com>
2023-11-17 19:38:12 +0530

    Create rest.js

---
+++ rest.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+const rest = () => {
+  return (
+    <div classname="container">
+      <div classname ="card">Harsh</div>
+    </div>
+
+  );
+};
+export default rest;
+
+
